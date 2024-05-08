ecc60bc4aa5b4d7deb93864155b3161fded3a88a
Armando Di Santo <armando.di.santo@hotmail.com>
2024-05-08 12:50:17 +0200

    Selfwork Variabili

---
+++ main.js
@@ -0,0 +1,31 @@
+let num1 = 42;
+
+let num2 = 17;
+
+// Visualizza il valore di num1
+console.log("Il valore della variabile num1 è " + num1);
+
+// Visualizza il valore di num2
+console.log("Il valore della variabile num2 è " + num2);
+
+// Assegna una stringa alla variabile stringa
+let stringa = "Questo è un esempio di stringa.";
+
+// Visualizza il valore della variabile stringa
+console.log(stringa);
+
+// Assegna una nuova stringa alla variabile stringa
+stringa = "Questa è un'altra stringa.";
+
+// Visualizza il nuovo valore della variabile stringa
+console.log(stringa);
+
+// Dichiarazione della costante PIGRECO con il valore di π
+const PIGRECO = 3.14159265359;
+
+// Visualizza il valore della costante PIGRECO
+console.log(PIGRECO);
+
+// Tentativo di cambiare il valore di PIGRECO con risultato di errore "main.js:13 Uncaught TypeError: Assignment to constant variable. at main.js:13:9"
+PIGRECO = 1;
+
